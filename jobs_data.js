cfde9f9793749ab24ff9d58dcfdedda2d27c33e5
Smith <smithri@administrators-MacBook-Pro.local>
2016-11-02 15:25:27 -0400

    update jobs_data.js

--- jobs_data.js
+++ jobs_data.js
@@ -28,15 +28,34 @@ jobsWidget = {
 						title : jobListing.job_title,
 						salaryMin : jobListing.minimum_salary,
 						salaryMax : jobListing.maximum_salary,
-						department : jobListing.department
+						department : jobListing.department,
+
 					}
 					jobsWidget.openJobs.jobData.push(tempObject);
 				});
 				console.log(jobsWidget.openJobs.jobData);
 			});
 		},
-		getJobTalbeRowMarkup : function(){
+		getJobTableRowMarkup : function(){
+			var tableRowsDisplayed = 7; //number of rows that show up in the onebox
 
-		}		
+			//check the value of the dropdown
+			dropdownValue = $("#filter-jobsSelectBoxItText").text();
+			dropdownValue = dropdownValue.trim(); //get rid of the spaces at the start and end of the string
+		},
+		sortJobData : function(dropdownValue){
+
+			switch(dropdownValue){
+				//because we're comparing internal array properties, each sort type needs it's own compare function
+				case "Highest Salary":
+					jobsWidget.openJobs.jobData.sort(function(a,b) {return (a.salaryMax > b.salaryMax) ? 1 : ((b.salaryMax > a.salaryMax) ? -1 : 0);} );
+					console.log(jobsWidget.openJobs.jobData);
+					break;
+				case "Part-Time":
+					break;
+				case "Most Popular":
+					break;
+			}
+		}	
 	}
 }
