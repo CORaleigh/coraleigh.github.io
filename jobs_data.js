420e3a4c8989459533ad5be2177027b8971ec3dc
Smith <smithri@administrators-MacBook-Pro.local>
2016-11-02 14:01:51 -0400

    update jobs_data.js

---
+++ jobs_data.js
@@ -0,0 +1,28 @@
+jobsWidget = {
+	openJobs : {
+		jobData = [],
+		getTableRowMarkup : function(jobTitle,jobSalaryMin,jobSalaryMax,jobDepartment){
+			jobTableMarkup = 
+				"<tr class='gsa-table__row'>" +
+					"<td>"+
+						"<a href='#'>"+ jobTitle + "</a>" +
+					"</td>"+
+					"<td>" +jobSalaryMin +"-" + jobSalaryMax + "</td>" +
+					"<td>" + jobDepartment +"</td>" +
+				"</tr>";
+			return getTableRowMarkup;
+
+		}
+
+		getJobsData = function(){
+			$.ajax({
+				url: "https://data.raleighnc.gov/resource/a95t-r2n7.json",
+			}).done(function(data){
+				console.log(data);
+			});
+		},
+		getJobTalbeRowMarkup : function(){
+
+		}		
+	}
+}
