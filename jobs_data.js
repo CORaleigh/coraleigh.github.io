b756a7fa31ff977abb4b7f758849593e92c2abb9
Smith <smithri@administrators-MacBook-Pro.local>
2016-11-09 14:48:40 -0500

    update jobs_data.js

--- jobs_data.js
+++ jobs_data.js
@@ -1,6 +1,34 @@
 jobsWidget = {
 	openJobs : {
 		jobData : [],
+
+		sortJobData : function(dropdownValue){
+
+			switch(dropdownValue){
+				//because we're comparing internal array properties, each sort type needs it's own compare function
+				case "Highest Salary":
+					//first, we have to pull out the items that dont have a max salary, or have a salary of "Hourly"
+					var salariedJobs = [];
+					jobsWidget.openJobs.jobData.forEach(function(job){
+						console.log(job.salaryMax + "<-" + (typeof job.salaryMax));
+						console.log()
+						if(job.salaryMax && job.salaryMax != "Hourly" && (typeof job.salaryMax != "undefined")){ //if there's no value for salary max, ignore the value
+							salariedJobs.push(job); //add the job to the list of salaried jobs
+						}
+					});
+					function salarySort(a, b){
+					  return ((a.salaryMax < b.salaryMax) ? -1 : ((a.salaryMax > b.salaryMax) ? 1 : 0));
+					}
+					salariedJobs.sort(salarySort);
+					console.log(salariedJobs);
+					break;
+				case "Part-Time":
+					break;
+				case "Most Popular":
+					break;
+			}
+		},
+
 		getTableRowMarkup : function(jobTitle,jobSalaryMin,jobSalaryMax,jobDepartment){
 			jobTableMarkup = 
 				"<tr class='gsa-table__row'>" +
@@ -44,33 +72,8 @@ jobsWidget = {
 			//check the value of the dropdown
 			dropdownValue = $("#filter-jobsSelectBoxItText").text();
 			dropdownValue = dropdownValue.trim(); //get rid of the spaces at the start and end of the string
-		},
-		sortJobData : function(dropdownValue){
-
-			switch(dropdownValue){
-				//because we're comparing internal array properties, each sort type needs it's own compare function
-				case "Highest Salary":
-					//first, we have to pull out the items that dont have a max salary, or have a salary of "Hourly"
-					var salariedJobs = [];
-					jobsWidget.openJobs.jobData.forEach(function(job){
-						console.log(job.salaryMax + "<-" + (typeof job.salaryMax));
-						console.log()
-						if(job.salaryMax && job.salaryMax != "Hourly" && (typeof job.salaryMax != "undefined")){ //if there's no value for salary max, ignore the value
-							salariedJobs.push(job); //add the job to the list of salaried jobs
-						}
-					});
-					function salarySort(a, b){
-					  return ((a.salaryMax < b.salaryMax) ? -1 : ((a.salaryMax > b.salaryMax) ? 1 : 0));
-					}
-					salariedJobs.sort(salarySort);
-					console.log(salariedJobs);
-					break;
-				case "Part-Time":
-					break;
-				case "Most Popular":
-					break;
-			}
-		}	
+		}
+			
 	}
 }
 
