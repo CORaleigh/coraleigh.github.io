3aac8bf18bc5f9c2ea0c0bf49c9599774f107fdd
Smith <smithri@administrators-MacBook-Pro.local>
2016-11-14 11:28:30 -0500

    update jobs_data.js

--- jobs_data.js
+++ jobs_data.js
@@ -89,7 +89,9 @@ jobsWidget = {
 					var min = job.salaryMin;
 					var max = job.salaryMax;
 					var dept = job.department;
-					markup = jobsWidget.openJobs.getTableRowMarkup((url,title,min,job.max,dept));
+					markup = jobsWidget.openJobs.getTableRowMarkup((url,title,min,max,dept));
+					console.log(markup);
+
 					jQuery(".gsa-table tbody").append(jobsWidget.openJobs.getTableRowMarkup((job.url,job.title,job.salaryMin,job.salaryMax,job.department)));
 					tableRowsDisplayed = tableRowsDisplayed - 1;
 				}
