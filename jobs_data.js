b5831beb58fefe58abc5eb255cb0239e912024aa
Smith <smithri@administrators-MacBook-Pro.local>
2016-11-30 09:47:07 -0500

    changed cor.css

--- jobs_data.js
+++ jobs_data.js
@@ -1,3 +1,14 @@
+//Google Analytics Code
+//Pasted at the top to allow loading first
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-9880547-11', 'auto');
+ga('send', 'pageview');
+
 //number format function for the salary values
 Number.prototype.formatMoney = function(c, d, t){
 var n = this, 
@@ -235,3 +246,6 @@ $.ajax({
     window.onload = generateChart();
     window.onresize = generateChart;
 });
+
+  
+
