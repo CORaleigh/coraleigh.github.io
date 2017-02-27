f5623dab4ef2541a1fd7ea8eb2b010413961376a
Smith <smithri@administrators-MacBook-Pro.local>
2017-02-27 15:32:28 -0500

    changed js

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -7,6 +7,7 @@ $.ajax({
 	url : "https://data.raleighnc.gov/resource/3nnh-j2uc.json",
 }).done(function(topFormDownloads){
 	topFormDownloads.forEach(function(topFormDownload){
-		$(".cor-form-download-link-unset").text(topFormDownload.name).attr("href", topFormDownload.formurl.url);
+		var iconText = '<span class="file-downloads__icon"><span class="file-downloads__icon-text">Download</span><span class="gsa-icon-download gsa-icon--lrg"></span></span>'
+		$(".cor-form-download-link-unset").text(iconText + topFormDownload.name).attr("href", topFormDownload.formurl.url);
 	});
 });
