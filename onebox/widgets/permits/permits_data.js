7fa78cedfce7d0c52e7bf95f961d1b89c78ecf80
Smith <smithri@administrators-MacBook-Pro.local>
2017-02-27 15:13:07 -0500

    added test alert

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -1,1 +1,10 @@
-alert("woah");
+//as a reminder, these items are run when the file loads, so there is no need
+//for document.ready() for it to run
+
+//get the top permit downloads
+
+$.ajax({
+	url : "https://data.raleighnc.gov/resource/3nnh-j2uc.json",
+}).done(function(topFormDownloads)(){
+	console.log(topFormDownloads);
+});
