e59de647264aa4c92bdfe492032234f3536d24a6
Smith <smithri@administrators-MacBook-Pro.local>
2017-02-28 09:43:28 -0500

    changed js

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -8,6 +8,8 @@ $.ajax({
 }).done(function(topFormDownloads){
 	topFormDownloads.forEach(function(topFormDownload){
 		var iconText = '<span class="file-downloads__icon"><span class="file-downloads__icon-text">Download</span><span class="gsa-icon-download gsa-icon--lrg"></span></span>'
-		$(".cor-form-download-link-unset").text(iconText + topFormDownload.name).attr("href", topFormDownload.formurl.url);
+		$(".cor-form-download-link-unset").first().attr("href", topFormDownload.formurl.url);
+		$(".cor-form-download-link-unset").first().children(".cor-form-download-title").text(topFormDownload.name);
+		$(".cor-form-download-link-unset").first().removeClass("cor-form-download-link-unset");
 	});
 });
