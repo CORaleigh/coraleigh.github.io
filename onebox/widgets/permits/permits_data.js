7aeecca24d8b714964e6827e0efad2f16db37d44
Smith <smithri@administrators-MacBook-Pro.local>
2017-02-28 17:39:32 -0500

    changed js

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -50,7 +50,7 @@ $.ajax({
 	    var obj = workTypeCount;
 	    for (var key in obj) {
 	        if (obj.hasOwnProperty(key)) {
-	            arr.push(key + '=' + obj[key]);
+	            arr.push([Sting(key),obj[key]]);
 	        }
 	    };
 	    var result = arr.join(',');
