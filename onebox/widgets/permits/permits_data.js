bd3650dbd615c3e6997605c5f2423b3a3f8e8e99
Smith <smithri@administrators-MacBook-Pro.local>
2017-02-28 10:27:02 -0500

    changed js

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -7,7 +7,11 @@ $.ajax({
 	url : "https://data.raleighnc.gov/resource/3nnh-j2uc.json",
 }).done(function(topFormDownloads){
 	topFormDownloads.forEach(function(topFormDownload){
-		var iconText = '<span class="file-downloads__icon"><span class="file-downloads__icon-text">Download</span><span class="gsa-icon-download gsa-icon--lrg"></span></span>'
+		var permitRowText = '<li class="file-downloads__item"><a class='cor-form-download-link-unset' href="#" target="_blank"><span class="file-downloads__icon"><span class="file-downloads__icon-text">Download</span><span class="gsa-icon-download gsa-icon--lrg"></span></span><span class='cor-form-download-title'>Standard Residentail Review</span></a></li>';
+		var permittingLinkItems = $(".cor-form-download-link-unset");
+		if(permittingLinkItems.length === 0){
+			$("file-downloads").append(permitRowText);
+		}
 		$(".cor-form-download-link-unset").first().attr("href", topFormDownload.formurl.url);
 		$(".cor-form-download-link-unset").first().children(".cor-form-download-title").text(topFormDownload.name);
 		$(".cor-form-download-link-unset").first().removeClass("cor-form-download-link-unset");
