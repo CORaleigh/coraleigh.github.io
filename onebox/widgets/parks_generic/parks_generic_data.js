3f83816b6449556c7c4249cc11b4cb90c74c028c
Smith <smithri@administrators-MacBook-Pro.local>
2017-03-24 16:03:21 -0400

    added js

--- onebox/widgets/parks_generic/parks_generic_data.js
+++ onebox/widgets/parks_generic/parks_generic_data.js
@@ -1,6 +1,14 @@
 console.log("running");
 if(navigator.geolocation){
 	navigator.geolocation.getCurrentPosition(showPosition);
+	//get the list of all of the parks
+	$.ajax({
+		url : "https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
+	}).done(function(parks){
+		parks.features.forEach(function(park){
+			console.log(park);
+		})
+	});
 }else{
 	console.log("Unable to geolocate");
 }
@@ -9,3 +17,4 @@ function showPosition(pos){
 	console.log(pos.coords.latitude);
 	console.log(pos.coords.longitude)
 }
+
