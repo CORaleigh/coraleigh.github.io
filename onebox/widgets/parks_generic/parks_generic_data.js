338ed214ab1df00a1c6fccb0b674a4483fe15982
Smith <smithri@administrators-MacBook-Pro.local>
2017-03-24 16:05:38 -0400

    added js

--- onebox/widgets/parks_generic/parks_generic_data.js
+++ onebox/widgets/parks_generic/parks_generic_data.js
@@ -2,6 +2,15 @@ console.log("running");
 if(navigator.geolocation){
 	navigator.geolocation.getCurrentPosition(showPosition);
 	//get the list of all of the parks
+	
+}else{
+	console.log("Unable to geolocate");
+}
+
+function showPosition(pos){
+	console.log(pos.coords.latitude);
+	console.log(pos.coords.longitude);
+
 	$.ajax({
 		url : "https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
 	}).done(function(parks){
@@ -9,12 +18,5 @@ if(navigator.geolocation){
 			console.log(park);
 		})
 	});
-}else{
-	console.log("Unable to geolocate");
-}
-
-function showPosition(pos){
-	console.log(pos.coords.latitude);
-	console.log(pos.coords.longitude)
 }
 
