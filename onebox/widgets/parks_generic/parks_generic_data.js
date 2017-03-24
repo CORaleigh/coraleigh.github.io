e5d591c178de55156fbaaea35b267c3e14f5b904
Smith <smithri@administrators-MacBook-Pro.local>
2017-03-24 16:14:24 -0400

    added js

--- onebox/widgets/parks_generic/parks_generic_data.js
+++ onebox/widgets/parks_generic/parks_generic_data.js
@@ -14,6 +14,8 @@ function showPosition(pos){
 	$.ajax({
 		url : "https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
 	}).done(function(parks){
+		
+		var parksJSON = JSON.parse(parks);
 		console.log(parks);
 		parks.features.forEach(function(park){
 			console.log(park);
