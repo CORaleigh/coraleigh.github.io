fd4da1ae7dc401bdf5bf78228c7f93b18e7a8547
Smith <smithri@administrators-MacBook-Pro.local>
2017-04-10 16:43:46 -0400

    starting parks specific widget

--- onebox/widgets/parks_specific/parks_specific_data.js
+++ onebox/widgets/parks_specific/parks_specific_data.js
@@ -4,7 +4,6 @@ function getParkInfoFromObjectID(objectID,callback = false){
 	//add the object ID to the ajax string
 	var ajaxString = "https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=OBJECTID%3D" + objectID + "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson";
 
-	console.log(newAjaxString);
 	jQuery.ajax({
 		url : ajaxString
 	}).done(function(parkInfo){
