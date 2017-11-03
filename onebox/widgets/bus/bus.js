2d393618a0957ae9df597ae9d0220eaeb6414a1c
elbaumpj <elbaumpj@gmail.com>
2017-11-03 10:50:53 -0400

    editing bus template

--- onebox/widgets/bus/bus.js
+++ onebox/widgets/bus/bus.js
@@ -1,37 +1,14 @@
 $(document).ready(function(){
-    //write the code to fill the container div here
-    
-    const widgetMarkup = <div w3-include-html="https://coraleigh.github.io/onebox/widgets/bus/static-bus.html"></div>
 
 	$.get("https://coraleigh.github.io/onebox/widgets/jobs/jobsWidget.html",function(widgetMarkup){
 		$(".onloadFillThis").append(widgetMarkup);
 
-		$.getScript( "https://code.jquery.com/jquery-1.9.0.js",function(){
 
-			$.getScript( "https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js", function(){
+		$.getScript( "https://coraleigh.github.io/onebox/js/global.js", function(){
 
-				$.getScript( "https://coraleigh.github.io/js/jquery.selectBoxIt.min.js", function(){
-
-			        $.getScript( "https://www.gstatic.com/charts/loader.js",function(){
-						google.charts.load('current', {'packages':['corechart', 'bar']});
-
-						$.getScript( "https://coraleigh.github.io/onebox/js/global.js", function(){
-
-							$.getScript( "https://coraleigh.github.io/onebox/js/global.js", function(){
-
-
-							});
-
-						});
-					});
-
-				});
-
-			});
-			
-		});
 		
                 
-	})
-	console.log("COReady")
-});
+		}); 
+	
+	});
+}); 
