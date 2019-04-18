d5286759e64845474c834f01b18d8125d7294577
Rickey Smith <res0905@gmail.com>
2019-04-18 13:29:54 -0400

    removing debugger

--- onebox/widgets/permits/permits_data.js
+++ onebox/widgets/permits/permits_data.js
@@ -29,115 +29,115 @@
 	$.ajax({
 		url : "https://services.arcgis.com/v400IkDOw1ad7Yad/ArcGIS/rest/services/Building_Permits_Past_31_Days/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnGeometry=false&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=workclass&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22count%22%2C%0D%0A++++%22onStatisticField%22%3A+%22workclass%22%2C+%0D%0A++++%22outStatisticFieldName%22%3A+%22WorkClassCount%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=",
 	}).done(function(recentPermits){
-		var numberOfGlobalPermits = 0; //= recentPermits.length; //the number for permits
-		//get the full number of permits
-		recentPermits = JSON.parse(recentPermits);
-		// console.log(recentPermits);
-		recentPermits.features.forEach(function(recentPermit){
-			// console.log(recentPermit.properties.WorkClassCount);
-			numberOfGlobalPermits = numberOfGlobalPermits + parseInt(recentPermit.properties.WorkClassCount);
-		});
-
-		//get the types of each permit
-
-
-		//set up the permits chart
-		// if ( $('#' + $chartCommonPermitsContainer).length > 0 ) {
-
-			var workTypeCount = [];
-			// recentPermits.features.forEach(function(recentPermit){
-			// 	if(workTypeCount[recentPermit.workclass]){
-			// 		workTypeCount[   -.workclass]++;
-			// 	}else{
-			// 		workTypeCount[recentPermit.workclass] = 1;
-			// 	}
-
-			// });
-			// console.log(workTypeCount);
-
-			var arr = [];
-			// console.log(recentPermits)
-			recentPermits.features.forEach(function(recentPermit){
-				arr.push([recentPermit.properties.workclass,recentPermit.properties.WorkClassCount]);
-			})
-			var obj = workTypeCount;
-			for (var key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					arr.push([String(key),obj[key]]);
-				}
-			};
-			// var result = arr.join(',');
-			 googleChartWorkTypeCount = arr;
-			 dataCommonPermits = new google.visualization.DataTable();
-			dataCommonPermits.addColumn('string', 'Item');
-			dataCommonPermits.addColumn('number', 'Amount');
-
-			//sort the smaller types into a "remaining types" category
-			var consolidationFactor = 0.10 //anything under 10% gets moved to "remaining types"
-			var consolidationNumber = numberOfGlobalPermits * consolidationFactor; //if there are less than this number, then consolidate the section.
-			// console.log(consolidationNumber);
-			var numberOfConsolidatedPermits = 0;
-			var elementsToSplice = []
-			for(x = 0; x < googleChartWorkTypeCount.length; x++){
-				if(googleChartWorkTypeCount[x][1] < consolidationNumber){
-					//consolidate
-					numberOfConsolidatedPermits = numberOfConsolidatedPermits + googleChartWorkTypeCount[x][1];
-					elementsToSplice.push(x);
-					console.log("consolidating " + googleChartWorkTypeCount[x][0] + " for value of  " + googleChartWorkTypeCount[x][1]);
-				}
-			}
-			elementsToSplice.sort(function(a,b){return b-a});
-			while(elementsToSplice.length > 0){
-				console.log("removing" + googleChartWorkTypeCount[elementsToSplice[0]][0]);
-				googleChartWorkTypeCount.splice(elementsToSplice[0],1);
-				elementsToSplice.splice(0,1);
-			}
-
-			if(numberOfConsolidatedPermits > 0){
-				googleChartWorkTypeCount.push(['Remaining Types',numberOfConsolidatedPermits]);
-			}
-
-			//convert the array into something gcharts can digest
-			// var googleChartWorkTypeCount = [];
-			workTypeCount.forEach(function(key){
-				// console.log(key);
-				// console.log(value);
-				googleChartWorkTypeCount.push([String(key),value]);
-			});
-			// console.log(googleChartWorkTypeCount);
-			dataCommonPermits.addRows(googleChartWorkTypeCount);
-			// dataCommonPermits.addRows([
-			//     ['Home Additions', 500],
-			//     ['Sidewalks', 125],
-			//     ['Pools', 250]
-			// ]);
-
-
-			var optionsCommonPermits = {
-				// colors: ['#477dca', '#efb505', '#ffffff'], // blue | gold | white
-				fontSize: 13,
-				chartArea:{
-					left: '5%',
-					top: 20,
-					width: '90%',
-					height: '165',
-				},
-				legend: {
-					'position':'right',
-					textStyle: {
-						color: "fff"
-					}
-				},
-				backgroundColor: { fill:'3c61a5' }
-			};
-
-
-			var chartCommonPermits = new google.visualization.PieChart(document.getElementById('chart-common-permits'));
-				chartCommonPermits.draw(dataCommonPermits, optionsCommonPermits);
-			   console.log(numberOfGlobalPermits);
-			$(".cta-stats__item-count").text(numberOfGlobalPermits); //paint the total permit number
-		// }
-	});
+	// 	var numberOfGlobalPermits = 0; //= recentPermits.length; //the number for permits
+	// 	//get the full number of permits
+	// 	recentPermits = JSON.parse(recentPermits);
+	// 	// console.log(recentPermits);
+	// 	recentPermits.features.forEach(function(recentPermit){
+	// 		// console.log(recentPermit.properties.WorkClassCount);
+	// 		numberOfGlobalPermits = numberOfGlobalPermits + parseInt(recentPermit.properties.WorkClassCount);
+	// 	});
+	//
+	// 	//get the types of each permit
+	//
+	//
+	// 	//set up the permits chart
+	// 	// if ( $('#' + $chartCommonPermitsContainer).length > 0 ) {
+	//
+	// 		var workTypeCount = [];
+	// 		// recentPermits.features.forEach(function(recentPermit){
+	// 		// 	if(workTypeCount[recentPermit.workclass]){
+	// 		// 		workTypeCount[   -.workclass]++;
+	// 		// 	}else{
+	// 		// 		workTypeCount[recentPermit.workclass] = 1;
+	// 		// 	}
+	//
+	// 		// });
+	// 		// console.log(workTypeCount);
+	//
+	// 		var arr = [];
+	// 		// console.log(recentPermits)
+	// 		recentPermits.features.forEach(function(recentPermit){
+	// 			arr.push([recentPermit.properties.workclass,recentPermit.properties.WorkClassCount]);
+	// 		})
+	// 		var obj = workTypeCount;
+	// 		for (var key in obj) {
+	// 			if (obj.hasOwnProperty(key)) {
+	// 				arr.push([String(key),obj[key]]);
+	// 			}
+	// 		};
+	// 		// var result = arr.join(',');
+	// 		 googleChartWorkTypeCount = arr;
+	// 		 // dataCommonPermits = new google.visualization.DataTable();
+	// 		dataCommonPermits.addColumn('string', 'Item');
+	// 		dataCommonPermits.addColumn('number', 'Amount');
+	//
+	// 		//sort the smaller types into a "remaining types" category
+	// 		var consolidationFactor = 0.10 //anything under 10% gets moved to "remaining types"
+	// 		var consolidationNumber = numberOfGlobalPermits * consolidationFactor; //if there are less than this number, then consolidate the section.
+	// 		// console.log(consolidationNumber);
+	// 		var numberOfConsolidatedPermits = 0;
+	// 		var elementsToSplice = []
+	// 		for(x = 0; x < googleChartWorkTypeCount.length; x++){
+	// 			if(googleChartWorkTypeCount[x][1] < consolidationNumber){
+	// 				//consolidate
+	// 				numberOfConsolidatedPermits = numberOfConsolidatedPermits + googleChartWorkTypeCount[x][1];
+	// 				elementsToSplice.push(x);
+	// 				console.log("consolidating " + googleChartWorkTypeCount[x][0] + " for value of  " + googleChartWorkTypeCount[x][1]);
+	// 			}
+	// 		}
+	// 		elementsToSplice.sort(function(a,b){return b-a});
+	// 		while(elementsToSplice.length > 0){
+	// 			console.log("removing" + googleChartWorkTypeCount[elementsToSplice[0]][0]);
+	// 			googleChartWorkTypeCount.splice(elementsToSplice[0],1);
+	// 			elementsToSplice.splice(0,1);
+	// 		}
+	//
+	// 		if(numberOfConsolidatedPermits > 0){
+	// 			googleChartWorkTypeCount.push(['Remaining Types',numberOfConsolidatedPermits]);
+	// 		}
+	//
+	// 		//convert the array into something gcharts can digest
+	// 		// var googleChartWorkTypeCount = [];
+	// 		workTypeCount.forEach(function(key){
+	// 			// console.log(key);
+	// 			// console.log(value);
+	// 			googleChartWorkTypeCount.push([String(key),value]);
+	// 		});
+	// 		// console.log(googleChartWorkTypeCount);
+	// 		dataCommonPermits.addRows(googleChartWorkTypeCount);
+	// 		// dataCommonPermits.addRows([
+	// 		//     ['Home Additions', 500],
+	// 		//     ['Sidewalks', 125],
+	// 		//     ['Pools', 250]
+	// 		// ]);
+	//
+	//
+	// 		var optionsCommonPermits = {
+	// 			// colors: ['#477dca', '#efb505', '#ffffff'], // blue | gold | white
+	// 			fontSize: 13,
+	// 			chartArea:{
+	// 				left: '5%',
+	// 				top: 20,
+	// 				width: '90%',
+	// 				height: '165',
+	// 			},
+	// 			legend: {
+	// 				'position':'right',
+	// 				textStyle: {
+	// 					color: "fff"
+	// 				}
+	// 			},
+	// 			backgroundColor: { fill:'3c61a5' }
+	// 		};
+	//
+	//
+	// 		var chartCommonPermits = new google.visualization.PieChart(document.getElementById('chart-common-permits'));
+	// 			chartCommonPermits.draw(dataCommonPermits, optionsCommonPermits);
+	// 		   console.log(numberOfGlobalPermits);
+	// 		$(".cta-stats__item-count").text(numberOfGlobalPermits); //paint the total permit number
+	// 	// }
+	// });
 	//Chlorepleth Map CODE
 	google.charts.load('current', {'packages':['corechart']})
 // });
